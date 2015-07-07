5201e2121ba5fd26c5c25e29594ddb1187cb0c33
Ishil-Puri <ishil.puri@gmail.com>
2015-07-07 12:29:09 -0700

    Console_Lab

--- Labs/Console_Lab.java
+++ Labs/Console_Lab.java
@@ -15,134 +15,120 @@ public class Console_Lab {
 	 */
 	public static void main(String[] args) {
 		// Console Lab
-		console();
+		Scanner console = new Scanner (System.in);
+		console(console);
 		System.out.println();
-		oddeven();
+		oddeven(console);
 		System.out.println();
-		zero();
+		zero(console);
 		System.out.println();
-		string();
+		string(console);
 		System.out.println();
-		len();
+		len(console);
 		System.out.println();
-		ten();
+		ten(console);
 		System.out.println();
-		read();
+		read(console);
 		System.out.println();
 		random(1,5);
 		System.out.println();
-		guessing_game(1,10);
+		guessing_game(1,10, console);
+		console.close();
 	}
 	//#1.1
-	public static void console() {
-		Scanner console = new Scanner (System.in);
+	public static void console(Scanner console) {
 		int num;
 		System.out.println("Please input a number: ");
 		num = console.nextInt();
 		System.out.println("You typed "+num+".");
-		//console.close();
 	}
 	//#1.2
-	public static void oddeven() {
-		Scanner oddeven = new Scanner (System.in);
+	public static void oddeven(Scanner console) {
 		int num;
 		System.out.println("Please enter a number: ");
-		num = oddeven.nextInt();
+		num = console.nextInt();
 		if (num%2==0){
 			System.out.println("The number you entered is even.");
 		}else{
 			System.out.println("The number you entered is odd.");
 		}
-		//oddeven.close();
 	}
 	//#1.3
-	public static void zero() {
-		Scanner zero = new Scanner (System.in);
+	public static void zero(Scanner console) {
 		int num;
 		System.out.println("Please input a number: ");
-		num = zero.nextInt();
+		num = console.nextInt();
 		while (num!=0) {
 			System.out.println("Please input a number: ");
-			num = zero.nextInt();
+			num = console.nextInt();
 		}	
-		//zero.close();
 	}
 	//#2.1
-	public static void string() {
-		Scanner string = new Scanner (System.in);
+	public static void string(Scanner console) {
 		String con;
 		System.out.println("Please enter a string: ");
-		con = string.nextLine();
+		con = console.next();
 		System.out.println("You entered "+ con+".");
-		//string.close();
 	}
 	//#2.2
-	public static void len() {
-		Scanner length = new Scanner (System.in);
+	public static void len(Scanner console) {
 		String con;
 		System.out.println("Please input a string: ");
-		con = length.nextLine();
+		con = console.next();
 		System.out.println("You typed "+ con+".");
 		int len;
 		len = con.length();
 		System.out.println(con+" is "+len+" characters long.");
-		//length.close();
 	}
 	//#2.3
-	public static void ten() {
-		Scanner tenth = new Scanner (System.in);
+	public static void ten(Scanner console) {
 		String ten;
-		int num_chars;
 		System.out.println("Please enter a string: ");
-		ten = tenth.nextLine();
-		num_chars = ten.length();
-		while(num_chars!=10){
+		ten = console.next();
+		while(ten.length() != 10){
 			System.out.println("Please enter a string: ");
-			ten = tenth.nextLine();
+			ten = console.next();
 		}
-		//tenth.close();
 	}
 	//#3.1
-	public static void read() {
-		Scanner onefive = new Scanner (System.in);
+	public static void read(Scanner console) {
 		int one = 1;
 		System.out.println("Please enter a number: ");
-		one = onefive.nextInt();
+		one = console.nextInt();
 		if (one<=5) {
 			System.out.println("Thank you for entering a number between 1 and 5!");
 		}else{
 			System.out.println("Please enter a number between 1 and 5: ");
-			one = onefive.nextInt();
+			one = console.nextInt();
 		}
-		onefive.close();
 	}
 	//#3.2
 	public static int random(int min, int max) {
-		/*Random i = new Random();
-		System.out.println(i);
-		return random;*/
 		Random rand = new Random();
 		int randomNum = rand.nextInt((max - min) + 1) + min;
 		System.out.println(randomNum);
 		return randomNum;
 	}
 	//#3.3
-	public static int guessing_game(int min, int max) {
-		Scanner guess = new Scanner (System.in);
+	public static int guessing_game(int min, int max, Scanner console) {
 		int guessnum;
-		guessnum = guess.nextInt();
-		Random rand = new Random();
 		System.out.println("Guess a number between 1 and 10!");
+		guessnum = console.nextInt();
+		Random rand = new Random();
 		int randomNum = rand.nextInt((max - min) + 1) + min;
-		if (guessnum == randomNum) {
-			System.out.println("Good guess!");
-		}else{
-			System.out.println("Try again!");
-			System.out.println();
-			System.out.println("Guess a number between 1 and 10!");
-			guessnum = guess.nextInt();
+		while(guessnum!=randomNum){
+			if (guessnum == randomNum) {
+				System.out.println("Good guess!");
+			}else{
+				System.out.println("Try again!");
+				System.out.println();
+				System.out.println("Guess a number between 1 and 10!");
+				guessnum = console.nextInt();
+				if (guessnum == randomNum){
+					System.out.println("Good guess!");
+				}
+			}
 		}
-		guess.close();
 		return randomNum;
 	}
 }
